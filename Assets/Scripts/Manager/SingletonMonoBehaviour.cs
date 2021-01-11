38c5ee24bd52fc46e9a84402538e6f2b9bb35e32
片山凌 <ryotrumpet328@gmail.com>
2021-01-11 22:03:43 +0900

    マネージャクラスをアタッチしたGameObjectが存在しない場良い自動生成する処理を実装

--- Assets/Scripts/Manager/SingletonMonoBehaviour.cs
+++ Assets/Scripts/Manager/SingletonMonoBehaviour.cs
@@ -7,13 +7,18 @@ public abstract class SingletonMonoBehaviour<T> : MonoBehaviour where T : MonoBe
     private static T instance;
     public static T Instance
     {
-        get{
-            if (instance == null) {
+        get
+        {
+            if (instance == null)
+            {
                 Type t = typeof(T);
 
                 instance = (T)FindObjectOfType (t);
-                if (instance == null) {
-                    Debug.LogError (t + " をアタッチしているGameObjectはありません");
+                if (instance == null)
+                {
+                    //アタッチしているGameObjectがない場合は自動生成する
+                    GameObject go = new GameObject(t.FullName);
+                    instance = go.AddComponent<T>();
                 }
             }
 
@@ -21,17 +26,22 @@ public abstract class SingletonMonoBehaviour<T> : MonoBehaviour where T : MonoBe
         }
     }
 
-    virtual protected void Awake(){
+    virtual protected void Awake()
+    {
         // 他のゲームオブジェクトにアタッチされているか調べる
         // アタッチされている場合は破棄する。
         CheckInstance();
     }
 
-    protected bool CheckInstance(){
-        if (instance == null) {
+    protected bool CheckInstance()
+    {
+        if (instance == null)
+        {
             instance = this as T;
             return true;
-        } else if (Instance == this) {
+        }
+        else if (Instance == this)
+        {
             return true;
         }
         Destroy (this);
