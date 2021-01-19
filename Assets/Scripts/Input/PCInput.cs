8fe519cba9082af2da36b55e0830db056ecb8462
片山凌 <ryotrumpet328@gmail.com>
2021-01-19 10:34:06 +0900

    クラス宣言とフォルダ整理

---
+++ Assets/Scripts/Input/PCInput.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PCInput : IInput
+{
+    public void InputRight()
+    {
+
+    }
+    public void InputLeft()
+    {
+
+    }
+    public void InputUp()
+    {
+
+    }
+    public void InputDown()
+    {
+
+    }
+}
---
+++ Assets/Scripts/Interface/IInput.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public interface IInput
+{
+    void InputRight();
+    void InputLeft();
+    void InputUp();
+    void InputDown();
+
+}
