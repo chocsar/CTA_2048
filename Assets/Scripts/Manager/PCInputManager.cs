03eb349ae5f64080d166a63b6c72690c063cb478
片山凌 <ryotrumpet328@gmail.com>
2021-01-19 16:43:17 +0900

    PC用のInputManagerを作成

---
+++ Assets/Scripts/Manager/PCInputManager.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PCInputManager : SingletonMonoBehaviour<PCInputManager>, IInput
+{
+    public bool InputRight()
+    {
+        return Input.GetKeyDown(KeyCode.RightArrow);
+    }
+    public bool InputLeft()
+    {
+        return Input.GetKeyDown(KeyCode.LeftArrow);
+    }
+    public bool InputUp()
+    {
+        return Input.GetKeyDown(KeyCode.UpArrow);
+    }
+    public bool InputDown()
+    {
+        return Input.GetKeyDown(KeyCode.DownArrow);
+    }
+}
