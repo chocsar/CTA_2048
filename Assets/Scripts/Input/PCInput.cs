bb45b2aa5b1eea26646deee5accd1abba593c74d
片山凌 <ryotrumpet328@gmail.com>
2021-01-21 12:48:38 +0900

    PCInputクラス作成

---
+++ Assets/Scripts/Input/PCInput.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PCInput : IInput
+{
+    public int GetInput()
+    {
+        if (Input.GetKeyDown(KeyCode.RightArrow))
+        {
+            return 1;
+        }
+        else if (Input.GetKeyDown(KeyCode.LeftArrow))
+        {
+            return 2;
+        }
+        else if (Input.GetKeyDown(KeyCode.UpArrow))
+        {
+            return 3;
+        }
+        else if (Input.GetKeyDown(KeyCode.DownArrow))
+        {
+            return 4;
+        }
+        else
+        {
+            return 0;
+        }
+    }
+}
