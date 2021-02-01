66e31c481dcdffa49cfb78d4d794f07dfbefcde4
片山凌 <ryotrumpet328@gmail.com>
2021-02-01 11:19:24 +0900

    Resultシーンでもランキング機能実装

--- Assets/Scripts/Result.cs
+++ Assets/Scripts/Result.cs
@@ -1,14 +1,35 @@
 ﻿using UnityEngine;
 using UnityEngine.UI;
+using System.Collections.Generic;
 
 public class Result : MonoBehaviour
 {
-    [SerializeField]
-    private Text resultText;
+    [SerializeField] private Text resultText;
+    [SerializeField] private GameObject content;
+    [SerializeField] private GameObject rankingElementPrefab;
 
     private void Start()
     {
         resultText.text = ScoreManager.Instance.LoadScore().ToString();
+        SetRanking();
+    }
+
+    private void SetRanking()
+    {
+        List<int> ranking = ScoreManager.Instance.LoadRanking();
+
+        int rank = 0;
+        foreach (int score in ranking)
+        {
+            RankingElement rankingElement = Instantiate(rankingElementPrefab).GetComponent<RankingElement>();
+            rankingElement.transform.SetParent(content.transform);
+            rankingElement.transform.localScale = new Vector3(1, 1, 1);
+
+            rankingElement.SetScore(score);
+            rankingElement.SetRank(rank + 1);
+
+            rank++;
+        }
     }
 
     public void OnClickRetryButton()
